a5e426b103736a5140d623fe3727b1d92966e919
aziznal <aziznal.dev@gmail.com>
2024-01-30 18:29:07 +0300

    add supabase types and create basic room list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { Database } from "@/supabase/database.types";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+type Room = Database["public"]["Tables"]["rooms"]["Row"];
+
 export default function Home() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const { data, error } = await supabase.from("rooms").select("*");
+
+      if (error) {
+        console.log("error", error);
+        return;
+      }
+
+      setRooms(data);
+    };
+
+    fetchRooms();
+  }, [supabase]);
+
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1>Home</h1>
+    <div className="flex flex-col justify-center items-center h-full">
+      <div className="flex flex-col gap-2">
+        {rooms.map((room) => (
+          <div key={room.id} className="flex gap-2">
+            <span>{room.name}</span>
+            <span className="flex gap-1">
+              <span>{room.width}</span>-<span>{room.height}</span>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
