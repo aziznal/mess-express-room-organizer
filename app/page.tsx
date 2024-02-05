b308375c56d87293d5da4c047bdf3086b385dd00
aziznal <aziznal.dev@gmail.com>
2024-02-05 17:16:01 +0300

    set home page to redirect to /rooms url

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,5 @@
-"use client";
-
-import { Room } from "@/lib/type-helpers";
-import { createClient } from "@/utils/supabase/client";
-import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [rooms, setRooms] = useState<Room[]>([]);
-
-  const supabase = createClient();
-
-  useEffect(() => {
-    const fetchRooms = async () => {
-      const { data, error } = await supabase.from("rooms").select("*");
-
-      if (error) {
-        console.log("error", error);
-        return;
-      }
-
-      setRooms(data);
-    };
-
-    fetchRooms();
-  }, [supabase]);
-
-  return (
-    <div className="flex flex-col justify-center items-center h-full">
-      <div className="flex flex-col gap-2">
-        {rooms.map((room) => (
-          <div key={room.id} className="flex gap-2">
-            <span>{room.name}</span>
-            <span className="flex gap-1">
-              <span>{room.width}</span>-<span>{room.height}</span>
-            </span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  redirect("/rooms");
 }
