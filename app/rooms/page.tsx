b0033262ee26113dcfc933efcb178a05b21147fd
aziznal <aziznal.dev@gmail.com>
2024-02-05 17:45:04 +0300

    add dialog for creating new rooms

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CreateNewRoom from "@/components/create-new-room";
 import RoomGridItem, { RoomGridItemSkeleton } from "@/components/room";
 import ToggleDarkMode from "@/components/toggle-dark-mode";
 import { Button } from "@/components/ui/button";
@@ -7,12 +8,13 @@ import {
   useCreateRoomMutation,
   useDeleteRoomMutation,
   useGetRoomsQuery,
+  useUpdateRoomMutation,
 } from "@/lib/services/rooms-service";
-import { LucidePlus } from "lucide-react";
-
+import { NewRoom } from "@/lib/type-helpers";
 export default function RoomPage() {
   const roomsQuery = useGetRoomsQuery();
   const createRoomMutation = useCreateRoomMutation();
+  const updateRoomMutation = useUpdateRoomMutation();
   const deleteRoomMutaion = useDeleteRoomMutation();
 
   return (
@@ -21,19 +23,13 @@ export default function RoomPage() {
 
       <div className="text-2xl font-bold mt-16">Welcome to MessExpress™</div>
 
-      <Button
-        className="my-6"
+      <CreateNewRoom
         disabled={createRoomMutation.isPending}
-        onClick={() =>
-          createRoomMutation.mutate({
-            name: "ABC",
-            width: 123,
-            height: 123,
-          })
+        isLoading={createRoomMutation.isPending}
+        onNewRoomCreated={async (newRoom: NewRoom) =>
+          createRoomMutation.mutateAsync(newRoom)
         }
-      >
-        New Room <LucidePlus />
-      </Button>
+      />
 
       {/*  Room Grid */}
       <div className="mt-12 w-full flex flex-col gap-6 sm:flex-row sm:flex-wrap sm:gap-0">
---
+++ components/create-new-room.tsx
@@ -0,0 +1,144 @@
+import { LucideLoader2, LucidePlus } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { NewRoom } from "@/lib/type-helpers";
+import { useEffect, useState } from "react";
+
+type CreateNewRoomProps = {
+  disabled: boolean;
+  isLoading: boolean;
+  onNewRoomCreated: (newRoom: NewRoom) => Promise<void>;
+};
+
+const formSchema = z.object({
+  name: z.string(),
+  height: z.coerce.number().int().positive(),
+  width: z.coerce.number().int().positive(),
+});
+
+export default function CreateNewRoom(props: CreateNewRoomProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      height: 0,
+      width: 0,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    await props.onNewRoomCreated(values);
+
+    setIsDialogOpen(false);
+  };
+
+  useEffect(() => {
+    if (isDialogOpen) {
+      form.reset();
+    }
+  }, [isDialogOpen, form]);
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button className="my-6" disabled={props.disabled}>
+          New Room <LucidePlus />
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Room</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+
+                  <FormControl>
+                    <Input placeholder="Room name" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="width"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Width</FormLabel>
+
+                  <FormControl>
+                    <Input placeholder="0cm" {...field} type="number" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="height"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Height</FormLabel>
+
+                  <FormControl>
+                    <Input placeholder="0cm" {...field} type="number" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button
+                disabled={props.isLoading}
+                type="submit"
+                className="flex gap-1"
+              >
+                Submit
+                {props.disabled && <LucideLoader2 className="animate-spin" />}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
