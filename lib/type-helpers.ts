5aedd51850ca519056c3147e604e4c6ec899f40a
berdemozdemir <berdemozdemir@gmail.com>
2024-02-10 15:14:05 +0300

    add room item types

--- lib/type-helpers.ts
+++ lib/type-helpers.ts
@@ -3,3 +3,7 @@ import { Database } from "@/supabase/database.types";
 export type Room = Database["public"]["Tables"]["rooms"]["Row"];
 export type NewRoom = Database["public"]["Tables"]["rooms"]["Insert"];
 export type UpdatedRoom = Database["public"]["Tables"]["rooms"]["Update"];
+
+export type RoomItem = Database["public"]["Tables"]["items"]["Row"];
+export type NewRoomItem = Database["public"]["Tables"]["items"]["Insert"];
+export type UpdatedRoomItem = Database["public"]["Tables"]["items"]["Update"];
