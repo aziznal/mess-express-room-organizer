00ac80606cf5e7779d8caca9ced355c9a069227f
aziznal <aziznal.dev@gmail.com>
2024-02-13 19:43:07 +0300

    add conditional nextjs config to remove extra options during development

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": ["node_modules/canvas/**"],
+let nextConfig = {};
+
+if (process.env.NODE_ENV === "development") {
+  // no need to do anything here
+} else {
+  nextConfig = {
+    ...nextConfig,
+    experimental: {
+      outputFileTracingExcludes: {
+        "*": ["node_modules/canvas/**"],
+      },
     },
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, "canvas", "jsdom"];
-    return config;
-  },
-};
+    webpack: (config) => {
+      config.externals = [...config.externals, "canvas", "jsdom"];
+      return config;
+    },
+  };
+}
 
 export default nextConfig;
