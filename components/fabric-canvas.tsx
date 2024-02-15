9316279e7fa6b4a5801d65b5e7320abf758f8b9e
aziznal <aziznal.dev@gmail.com>
2024-02-15 18:46:13 +0300

    factor out zoom and pan handler functions into utils file

--- components/fabric-canvas.tsx
+++ components/fabric-canvas.tsx
@@ -1,11 +1,10 @@
-// @ts-nocheck
-
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 
 import { Room } from "@/lib/type-helpers";
+import { setupPanHandler, setupZoomHandler } from "@/lib/canvas/utils";
 
 type FabricCanvasProps = {
   room: Room;
@@ -13,6 +12,8 @@ type FabricCanvasProps = {
 
 const FabricCanvas = (props: FabricCanvasProps) => {
   const canvasRef = useRef(null);
+
+  const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(null);
 
   useEffect(() => {
     if (!window) return;
@@ -22,78 +23,9 @@ const FabricCanvas = (props: FabricCanvasProps) => {
       width: window.innerWidth,
     });
 
-    canvas.on("mouse:wheel", function (opt) {
-      var delta = opt.e.deltaY;
-      var zoom = canvas.getZoom();
-
-      zoom *= 0.999 ** delta;
-
-      const maxZoomIn = 20;
-      const maxZoomOut = 0.001;
-
-      if (zoom > maxZoomIn) zoom = maxZoomIn;
-      if (zoom < maxZoomOut) zoom = maxZoomOut;
-
-      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-
-      opt.e.preventDefault();
-      opt.e.stopPropagation();
-
-      const vpt = this.viewportTransform;
-
-      // Supposed to prevent panning out of bounds but doesn't seem to work
-      // if (zoom < 400 / 1000) {
-      //   vpt[4] = 200 - (1000 * zoom) / 2;
-      //   vpt[5] = 200 - (1000 * zoom) / 2;
-      // } else {
-      //   if (vpt[4] >= 0) {
-      //     vpt[4] = 0;
-      //   } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
-      //     vpt[4] = canvas.getWidth() - 1000 * zoom;
-      //   }
-      //   if (vpt[5] >= 0) {
-      //     vpt[5] = 0;
-      //   } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
-      //     vpt[5] = canvas.getHeight() - 1000 * zoom;
-      //   }
-      // }
-    });
-
-    canvas.on("mouse:down", function (opt) {
-      const evt = opt.e;
-
-      if (evt.altKey === true) {
-        this.isDragging = true;
-        this.selection = false;
-        this.lastPosX = evt.clientX;
-        this.lastPosY = evt.clientY;
-      }
-    });
-
-    canvas.on("mouse:move", function (opt) {
-      if (this.isDragging) {
-        canvas.setCursor("grab");
-
-        var e = opt.e;
-        var vpt = this.viewportTransform;
-
-        vpt[4] += e.clientX - this.lastPosX;
-        vpt[5] += e.clientY - this.lastPosY;
-
-        this.requestRenderAll();
-
-        this.lastPosX = e.clientX;
-        this.lastPosY = e.clientY;
-      }
-    });
-
-    canvas.on("mouse:up", function (opt) {
-      // on mouse up we want to recalculate new interaction
-      // for all objects, so we call setViewportTransform
-      this.setViewportTransform(this.viewportTransform);
-      this.isDragging = false;
-      this.selection = true;
-    });
+    setFabricCanvas(canvas);
+    setupZoomHandler(canvas);
+    setupPanHandler(canvas);
 
     const roomRect = new fabric.Rect({
       width: props.room.width,
---
+++ lib/canvas/utils.ts
@@ -0,0 +1,58 @@
+// @ts-nocheck
+export const setupZoomHandler = (canvas: fabric.Canvas) => {
+  canvas.on("mouse:wheel", function (opt) {
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+
+    zoom *= 0.999 ** delta;
+
+    const maxZoomIn = 20;
+    const maxZoomOut = 0.001;
+
+    if (zoom > maxZoomIn) zoom = maxZoomIn;
+    if (zoom < maxZoomOut) zoom = maxZoomOut;
+
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+  });
+};
+
+export const setupPanHandler = (canvas: fabric.Canvas) => {
+  canvas.on("mouse:down", function (opt) {
+    const evt = opt.e;
+
+    if (evt.altKey === true) {
+      this.isDragging = true;
+      this.selection = false;
+      this.lastPosX = evt.clientX;
+      this.lastPosY = evt.clientY;
+    }
+  });
+
+  canvas.on("mouse:move", function (opt) {
+    if (this.isDragging) {
+      canvas.setCursor("grab");
+
+      var e = opt.e;
+      var vpt = this.viewportTransform;
+
+      vpt[4] += e.clientX - this.lastPosX;
+      vpt[5] += e.clientY - this.lastPosY;
+
+      this.requestRenderAll();
+
+      this.lastPosX = e.clientX;
+      this.lastPosY = e.clientY;
+    }
+  });
+
+  canvas.on("mouse:up", function (opt) {
+    // on mouse up we want to recalculate new interaction
+    // for all objects, so we call setViewportTransform
+    this.setViewportTransform(this.viewportTransform);
+    this.isDragging = false;
+    this.selection = true;
+  });
+};
